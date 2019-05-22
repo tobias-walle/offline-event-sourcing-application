e71f5a4654e1777239391c647631ea8f172b199e
Tobias Walle <tobias.walle@maibornwolff.de>
2019-05-22 21:43:54 +0200

    add client to build process

--- webpack.config.ts
+++ webpack.config.ts
@@ -10,7 +10,7 @@ const webpackConfig: Configuration = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist', 'client')
   },
   devServer: {
     proxy: {
